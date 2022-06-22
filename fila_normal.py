bdb4b48d1c42066fa34c8742edb825d45110da96
edenbam <edenbam@gmail.com>
2022-06-22 10:34:22 -0300

    Criação dos demais métodos de fila_normal e módulo de teste

--- fila_normal.py
+++ fila_normal.py
@@ -4,5 +4,18 @@ class filanormal:
     clientesatendidos = []
     senhaatual:str = ''
 
-    def gerasenhaatual(self) -> None:
+    def gerasenhaatual(self)->None:
         self.senhaatual = f'NM{self.codigo}'
+    def resetafila(self)->None:
+        if self.codigo >= 100:
+            self.codigo = 0
+        else:
+            self.codigo += 1
+    def atualizafila(self)->None:
+        self.resetafila()
+        self.gerasenhaatual()
+        self.fila.append(self.senhaatual)
+    def chamacliente(self,caixa:int)->str:
+        cliente_atual = self.fila.pop(0)
+        self.clientesatendidos.append(cliente_atual)
+        return (f'Cliente Atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
---
+++ main.py
@@ -0,0 +1,8 @@
+from fila_normal import filanormal
+
+fila_teste = filanormal()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+print(fila_teste.chamacliente(5))
+print(fila_teste.chamacliente(10))
